1f19595d7e026438e92dd3d95920134482991dbb
Arunthathy <your.arundathipk@gmail.com>
2024-10-20 00:39:19 +0530

    Create custom hook useLocalStorage to store data in local storage

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import '../assets/boardStyle.css'
 import Column from './Column'
+import {useLocalStorage} from '../hooks/useLocalStorage'
+
+const initialColumns = {
+    'To Do': [],
+    'In Progress': [],
+    'Done': []
+  };
 
 const Board = () => {
-  return (
-    <div className='board'>
-      <Column />
-      <Column />
-      <Column />
-    </div>
-  )
+    const [columns, setColumns] = useLocalStorage('kanbanColumns', initialColumns);
+    return (
+        <div className='board'>
+            {Object.keys(columns).map(column => (
+                <Column key={column} name={column} task={columns[column]} moveTask='' />
+            ))}
+        </div>
+    )
 }
 
 export default Board
