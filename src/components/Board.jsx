e7bf44e0fa41acd926e82d3e66db99f82651a034
Arunthathy <your.arundathipk@gmail.com>
2024-10-19 21:15:57 +0530

    Create initial folder structure for Kanban board with dummy  header and columns

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/boardStyle.css'
+const Board = () => {
+  return (
+    <div className='board'>
+      <div className='column'></div>
+      <div className='column'></div>
+      <div className='column'></div>
+    </div>
+  )
+}
+
+export default Board
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/headerStyle.css'
+const Header = () => {
+  return (
+    <header className='header'>
+        <h1>Kanban Board</h1>
+        <button></button>
+    </header>
+  )
+}
+
+export default Header
