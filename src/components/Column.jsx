b83389d414b9c304ed0767cdf39eb90baeebdc98
Arunthathy <your.arundathipk@gmail.com>
2024-10-19 21:46:02 +0530

    Create Task componet

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Task from './Task'
 
 const Column = () => {
   return (
     <div className="column">
         <h1>Name</h1>
-        <div>Tasks</div>
+        <Task />
     </div>
   )
 }
